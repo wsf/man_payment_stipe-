3f5ba44a4571bc44b0dffacfded75d475be1c7ff
Nelson Tontarelli <nelartz01@gmail.com>
2023-03-02 13:06:25 -0300

    Calcular promociones funcionando correctamente

--- static/src/js/promo.js
+++ static/src/js/promo.js
@@ -15,29 +15,29 @@ odoo.define('payment_stripe.stripe', function (require) {
         // paso 1: averiguo cual es la promo que tiene el tilde y le tomo los datos de la promo.
         // 1.1 recorro todos los select que hay
 
-        var datos_promo_seleccionada;
-        $('input[type=checkbox]:checked').each(function() {
-                  datos_promo_seleccionada = $(this).val();
-
-                  var due = parseFloat(datos_promo_seleccionada.split(",")[0].split(":")[1]);
-                  var tax = parseFloat(datos_promo_seleccionada.split(",")[1].split(":")[1]).toFixed(2);
-                  var total_carrito  = $("#order_total strong.monetary_field").text();
-                  var total = parseFloat(total_carrito.replace("$","").replace(",",".")).toFixed(2);
-
-                  // calculamos las cuotas según los intereses
-                  // calculamos las cuotas según los intereses
-                var ttotal, importe_cuotas = 0;
-
-                ttotal = (total * ((tax / 100) + 1)).toFixed(2);
-                importe_cuotas = (ttotal / due).toFixed(2).toString().replace(".",",");
-                ttotal = ttotal.toString().replace(".",",")
-
-                $("#cuota").text("> Cantidad de cuotas: " + due)
-                $("#importe").text("> Pago mensual: $ " + importe_cuotas)
-                $("#total").text("> Total a pagar: $ " + ttotal)
-
-                  console.log("Checkbox " + $(this).prop("id") +  " (" + datos_promo_seleccionada + ") Seleccionado");
-              });
+//        var datos_promo_seleccionada;
+//        $('input[type=checkbox]:checked').each(function() {
+//                  datos_promo_seleccionada = $(this).val();
+//
+//                  var due = parseFloat(datos_promo_seleccionada.split(",")[0].split(":")[1]);
+//                  var tax = parseFloat(datos_promo_seleccionada.split(",")[1].split(":")[1]).toFixed(2);
+//                  var total_carrito  = $("#order_total strong.monetary_field").text();
+//                  var total = parseFloat(total_carrito.replace("$","").replace(",",".")).toFixed(2);
+//
+//                  // calculamos las cuotas según los intereses
+//                  // calculamos las cuotas según los intereses
+//                var ttotal, importe_cuotas = 0;
+//
+//                ttotal = (total * ((tax / 100) + 1)).toFixed(2);
+//                importe_cuotas = (ttotal / due).toFixed(2).toString().replace(".",",");
+//                ttotal = ttotal.toString().replace(".",",")
+//
+//                $("#cuota").text("> Cantidad de cuotas: " + due)
+//                $("#importe").text("> Pago mensual: $ " + importe_cuotas)
+//                $("#total").text("> Total a pagar: $ " + ttotal)
+//
+//                  console.log("Checkbox " + $(this).prop("id") +  " (" + datos_promo_seleccionada + ") Seleccionado");
+//              });
 
         // 1.2 si el select está tildado le tomo los datos y lo guardo en una variables llamada: datos_promo_seleccionada
 
@@ -47,15 +47,61 @@ odoo.define('payment_stripe.stripe', function (require) {
 
 
         // ---- pruebas de Ale
-        var r_select_promo = $("#promo10").text()
-        console.log(" -- Leyendo la promo 10 -> " + r_select_promo);
+//        var r_select_promo = $("#promo10").text()
+//        console.log(" -- Leyendo la promo 10 -> " + r_select_promo);
 
   		//alert($("#order_total strong.monetary_field").text());
 
   		// veo si un checkbox está tildado.
-  		var r = $('#rdb_promo10').is(':checked')
+//  		var r = $('#rdb_promo10').is(':checked')
 
   });
+
+
+    //Pruebas JR
+
+    $("#promos_select").change(function(){
+        let option = $('select option:selected').val();
+        console.log(option)
+
+        if(option != 0){
+            var due = parseFloat(option.split(",")[0].split(":")[1]);
+              console.log('Cuotas', due)
+              var tax = parseFloat(option.split(",")[1].split(":")[1]).toFixed(2);
+              console.log('Impuesto', tax)
+              var total_carrito  = $("#order_total strong.monetary_field").text();
+              console.log('Total del Carrito', total_carrito)
+              var total = parseFloat(total_carrito.replace("$","").replace(",",".")).toFixed(2);
+              console.log('Total', total)
+
+              // calculamos las cuotas según los intereses
+              // calculamos las cuotas según los intereses
+            var ttotal, importe_cuotas = 0;
+
+            ttotal = (total * ((tax / 100) + 1)).toFixed(2);
+            importe_cuotas = (ttotal / due).toFixed(2).toString().replace(".",",");
+            ttotal = ttotal.toString().replace(".",",")
+
+            console.log(ttotal)
+
+            $("#cuota").text("> Cantidad de cuotas: " + due)
+            $("#importe").text("> Pago mensual: $ " + importe_cuotas)
+            $("#total").text("> Total a pagar: $ " + ttotal)
+        }else{
+            console.log("la opcion es 0:", option)
+
+
+            $("#cuota").text("> Cantidad de cuotas: " + "")
+            $("#importe").text("> Pago mensual: $ " + "")
+            $("#total").text("> Total a pagar: $ " + "")
+
+        }
+
+
+        })
+
+
+    console.log(option)
 
 
 
